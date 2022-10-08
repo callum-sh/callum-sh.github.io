3d50376df861e1e6c73a69cc4b92ca1a06ff7c89
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2022-10-08 11:27:47 -0400

    Updates

--- precache-manifest.0e55245dd0857e904f92f17ee0ec5bf2.js
+++ precache-manifest.0e55245dd0857e904f92f17ee0ec5bf2.js
@@ -20,28 +20,28 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/Agustina.21f233e1.woff"
   },
   {
-    "revision": "4457817ac2b9993c65e81aa05828fe9c",
-    "url": "/portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
-  },
-  {
-    "revision": "ee6539921d713482b8ccd4d0d23961bb",
-    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
   },
   {
     "revision": "90773b6158663ab0fe78b32680733677",
     "url": "/portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
   },
   {
-    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
-    "url": "/portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
   },
   {
-    "revision": "8d57e4014b18edef070d285746485115",
-    "url": "/portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
   },
   {
     "revision": "8fd3737925e83c87d78a13700ccf9a62",
     "url": "/portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
   },
   {
     "revision": "b5c77a6aed75cdad9489effd0d5ea411",
@@ -188,16 +188,16 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/fa-brands-400.06147b6c.ttf"
   },
   {
-    "revision": "f5f2566b93e89391da4db79462b8078b",
-    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
-  },
-  {
     "revision": "c1a866ec0e04a5e1915b41fcf261457c",
     "url": "/portfolio/static/media/fa-regular-400.c1a866ec.eot"
   },
   {
     "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
     "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
   },
   {
     "revision": "65b286af947c0d982ca01b40e1fcab38",
@@ -208,16 +208,16 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/fa-regular-400.7b9568e6.svg"
   },
   {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
     "revision": "44d537ab79f921fde5a28b2c1636f397",
     "url": "/portfolio/static/media/fa-solid-900.44d537ab.woff2"
   },
   {
     "revision": "333bae208dc363746961b234ff6c2500",
     "url": "/portfolio/static/media/fa-solid-900.333bae20.woff"
-  },
-  {
-    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
-    "url": "/portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
   },
   {
     "revision": "0bff33a5fd7ec390235476b4859747a0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.94de42f887909f94e6c343241784b423.js"
+  "/portfolio/precache-manifest.0e55245dd0857e904f92f17ee0ec5bf2.js"
 );
 
 workbox.clientsClaim();
